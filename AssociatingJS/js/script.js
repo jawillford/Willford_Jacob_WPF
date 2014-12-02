9df76976c35a84905d5568672a63d3c735a37fad
Jacob Willford <jawillford@fullsail.edu>
2014-12-02 14:40:17 -0600

    Basic HTML and JS files

---
+++ AssociatingJS/js/script.js
@@ -0,0 +1,11 @@
+alert("This is in the js file");
+
+var muppets;
+muppets = 1;
+
+var muppetNames = ["Kurmit", "Miss Piggy"];
+muppetNames[0] = "Rizzo";
+muppetNames[1] = "Fozzy";
+
+console.log(muppetNames);
+console.log(muppetNames[0]);
