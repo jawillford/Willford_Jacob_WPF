51d0a17fa68291815c458297ad09a3fb068858a2
Jacob Willford <jawillford@fullsail.edu>
2014-12-04 15:12:35 -0600

    Prompt and Alert follow along

---
+++ Prompt and Alert/js/script.js
@@ -0,0 +1,8 @@
+//Prompt and Alert
+
+var width = prompt("Enter the width of the rectangle:");
+var height = prompt("Enter the height of the rectangle:");
+var area = width * height;
+console.log(area);
+
+alert("The area of your rectangle is: " + area + " sq feet");
