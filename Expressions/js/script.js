210046d729746cf1ba5f9616998b2146f981f692
Jacob Willford <jawillford@fullsail.edu>
2014-12-04 14:54:05 -0600

    Expressions follow along

---
+++ Expressions/js/script.js
@@ -0,0 +1,41 @@
+//Expressions**
+
+var quiz1 = 87;
+var quiz2 = 100;
+var quiz3 = 60;
+var quiz4 = 80;
+var average = (quiz1 + quiz2 + quiz3 + quiz4) / 4;
+console.log(average);
+
+var length = 7;
+var width = 6;
+var perimeter = length * 2 + width * 2;
+console.log(perimeter);
+
+var orangeBins = [234, 567, 883];
+var total = orangeBins[0] + orangeBins[1] + orangeBins[2];
+console.log(total);
+
+var firstName = "Jacob";
+var lastName = "Willford";
+var fullName = firstName + " " + lastName;
+console.log(fullName);
+
+var a = "6";
+var b = "7";
+var result = a + b;
+console.log(result);
+
+var stringVar = "6";
+var result1 = 7 + Number(stringVar);
+console.log(result1);
+
+var areaCode = 407;
+var firstPart = 203;
+var secondPart = 4529;
+var phoneNumber = "(" + String(areaCode) + ") " + String(firstPart) + " " + String(secondPart);
+console.log(phoneNumber);
+
+var a1 = 3;
+a1++;
+console.log(a1);
