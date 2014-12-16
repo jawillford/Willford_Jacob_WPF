5e84e3bb8686b7bcfcc3b246a793fdf372f46d87
Jacob Willford <jawillford@fullsail.edu>
2014-12-15 23:11:37 -0600

    Follow along

---
+++ Loops/js/script.js
@@ -0,0 +1,17 @@
+/**
+ * Created by jacobawillford on 12/15/14.
+ */
+
+a = 10;
+while(a > 0) {
+    console.log(a);
+    a--;
+}
+var c = 10;
+do{
+    console.log(c);
+    c --;
+}
+while(c > 0);
+
+for(i = 10; i > 0; i --)console.log(i);
