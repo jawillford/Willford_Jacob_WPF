b377820d27663220d26db24946132c22c6ce9712
Jacob Willford <jawillford@fullsail.edu>
2014-12-15 22:16:54 -0600

    Follow along

---
+++ Functions/js/script.js
@@ -0,0 +1,18 @@
+/**
+ * Created by jacobawillford on 12/15/14.
+ */
+
+function dogYears(age){
+    var dogYears = age * 7;
+    console.log("Sparky is " + dogYears + " years old.")
+}
+var age = 4;
+dogYears(age);
+dogYears(5);
+
+
+function calcArea(w, h){
+    var area = w * h;
+    console.log(area);
+}
+calcArea(20, 30);
