53b15dd205418d6c097d2c88217f88a2715b0af1
Jacob Willford <jawillford@fullsail.edu>
2014-12-16 07:51:31 -0600

    Changed function

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -4,10 +4,17 @@
 var limit = Number(prompt("How many people can your server handle on your game at once?"));
 var people = Number(prompt("How many people are currently on your game?"));
 var maxRate = Number(prompt("How many people can your server handle accessing your game per second?"));
-var rateOfPeople = Number(prompt("How many more people are accessing your game per second?"));
-if(people >= limit || rateOfPeople >= maxRate){
-    alert("You should add a que to access your game in order to prevent your servers from crashing.");
-}
+var rateOfPeople = Number(prompt("How many more people are accessing your game per minute?"));
+
+myFunction = function(limit, people, rateOfPeople){
+    var spaceLeft = limit - people;
+    var timeLeft = spaceLeft / rateOfPeople;
+    alert("Your servers will be full in " + timeLeft + " minute.");
+};
+myFunction (limit, people, rateOfPeople);
+
+if(people >= limit || rateOfPeople >= maxRate)alert("You should add a que to access your game in order to prevent your servers from crashing.");
+
 while(people < limit){
     console.log(people + " people are currently on your game.");
     people = people + rateOfPeople;
